927142b5d54bb1a90d99f35400c2057a71b482b1
J <j.mey.r8@gmail.com>
2019-05-27 17:51:33 -0400

    array items working to produce gifs

---
+++ assets/javascript/game.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+var heroes = ["Goku", "Spider-Man", "Ironman", "One Punch Man"];
+
+// function to display hero data
+function renderButtons(){
+
+    // 
+    $("#button-view").empty();
+
+    // looping through the array of topics
+    for (var i=0; i < heroes.length; i++) {
+        
+        // dynamic button generated
+        var a = $("<button>");
+        // adding a class
+        a.addClass("hero");
+        // adding data-attribute with value of heroes at index i
+        a.attr("hero-name", heroes[i]);
+        // adding text value to button
+        a.text(heroes[i]);
+        // adding button to the HTML
+        $("#button-view").append(a);
+    }
+}
+
+renderButtons();
+
+// Adding button for heroes 
+$("#add-hero").on("click", function(){
+
+    event.preventDefault();
+
+    var hero = $("#hero-input").val().trim();
+
+    heroes.push(hero);
+
+    renderButtons();
+})
+
+// Pulling hero gifs from giphy api
+
+$("button").on("click", function(){
+
+    var hero = $(this).attr("hero-name");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        hero + "&api_key=aV4XV0UMMkbHJCOI3nWb5fraoWtOZrqc&limit=10";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+
+    .then(function(response){
+        //console.log (queryURL);
+        //console.log (response);
+
+        var results = response.data;
+
+        for(var i=0; i < results.length; i++) {
+            var heroDiv = $("<div>");
+
+            //var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            var heroImage = $("<img>");
+
+            heroImage.attr("src", results[i].images.fixed_height.url);
+
+            heroDiv.append(p);
+            heroDiv.append(heroImage);
+
+            $("#heroes-view").append(heroDiv);
+        }
+    });
+});
+
+});
+
+
