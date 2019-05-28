053d10949bf4991c3901fc96cc396cf7472b1064
J <j.mey.r8@gmail.com>
2019-05-27 23:40:01 -0400

    working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var heroes = ["Goku", "Spider-Man", "Ironman", "One Punch Man"];
+var topics = ["Son Goku", "Spider-Man", "Ironman", "One Punch Man", "John Wick"];
 
 // function to display hero data
 function renderButtons(){
@@ -9,16 +9,16 @@ function renderButtons(){
     $("#button-view").empty();
 
     // looping through the array of topics
-    for (i=0; i < heroes.length; i++) {
+    for (i=0; i < topics.length; i++) {
         
         // dynamic button generated
         var a = $("<button>");
         // adding a class
         a.addClass("hero");
-        // adding data-attribute with value of heroes at index i
-        a.attr("hero-name", heroes[i]);
+        // adding data-attribute with value of topics at index i
+        a.attr("hero-name", topics[i]);
         // adding text value to button
-        a.text(heroes[i]);
+        a.text(topics[i]);
         // adding button to the HTML
         $("#button-view").append(a);
     }
@@ -31,13 +31,13 @@ $("#add-hero").on("click", function(event){
 
     var hero = $("#hero-input").val().trim();
 
-    heroes.push(hero);
+    topics.push(hero);
 
     $("hero-input").val("");
 
     renderButtons();
 
-    console.log(heroes);
+    console.log(topics);
 })
 
 renderButtons();
@@ -58,31 +58,56 @@ $.ajax({
 })
 
     .then(function(response){
-        //console.log (queryURL);
-        //console.log (response);
-
+        
         var results = response.data;
 
         $("#heroes-view").empty();
 
         for (var i=0; i < results.length; i++) {
 
+            // creating tempory div to place rating and image in HTML
             var heroDiv = $("<div>");
 
-            //var rating = results[i].rating;
-
+            // creating tempory p for ratings
             var p = $("<p>").text("Rating: " + results[i].rating);
 
+            // creating tempory img for gifs
             var heroImage = $("<img>");
 
+            // adding attributes to the images pull from giphy
+            heroImage.attr("data-still", results[i].images.fixed_height_still.url);
+            heroImage.attr("data-animate", results[i].images.fixed_height.url);
             heroImage.attr("src", results[i].images.fixed_height.url);
+            heroImage.attr("data-state", "still");
 
+            // On click function to stop animation
+            heroImage.on("click", stopAnimation);
+
+            // appending ratings and images to heroDiv
             heroDiv.append(p);
             heroDiv.append(heroImage);
 
+            // appending heroDiv to the HTML
             $("#heroes-view").append(heroDiv);
         }
     });
+
+    // function to stop animation 
+    function stopAnimation (){
+        var state = $(this).attr("data-state");
+        var animate = $(this).attr("data-animate");
+        var still = $(this).attr("data-still");
+        
+        if (state === "still") {
+            $(this).attr("src", animate);
+            $(this).attr("data-state", "animate");
+        }else{
+            $(this).attr("src", still);
+            $(this).attr("data-state", "still");
+        }
+    }
+
+
 });
 
 
